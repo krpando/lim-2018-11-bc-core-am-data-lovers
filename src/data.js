81b56c5bbd86b7d25d3b5363ce5b9db06825497c
krpando <karpando@gmail.com>
2018-12-15 14:52:06 -0500

    CSS & HTML (First mobile), landing page

--- src/data.js
+++ src/data.js
@@ -1,8 +1,28 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
+/*const example = () => {
   return 'example';
 };
 
-window.example = example;
+window.example = example;*/
+const primerPaso =WORLDBANK.PER.indicators;
+let resultadoPrimerPaso=[];
+for(let i=0;i<primerPaso.length;i++){
+  resultadoPrimerPaso.push(primerPaso[i]);
+  }
+let segundoPaso = [];
+for(var i=0;i<resultadoPrimerPaso.length;i++){
+segundoPaso.push(resultadoPrimerPaso[i].indicatorCode);
+  }
+let palabra = document.getElementById("palabra").value
+if(palabra==="indicadores"){
+  document.write(segundoPaso.join(""));
+  }
+  window.filter(indicadores,search) = {
+    if(indicadores){
+      if(search){
+
+      }
+    }
+  }
