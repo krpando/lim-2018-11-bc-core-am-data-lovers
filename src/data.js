bc0818728f248a20f909ecf30888bdf061b667f6
krpando <karpando@gmail.com>
2018-12-19 08:52:45 -0500

    probando filter(test)

--- src/data.js
+++ src/data.js
@@ -1,50 +1,55 @@
+const busqueda = () => {
+ 
+};
+
+window.WORLDBANK = {
+  busqueda
+};
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-/*const example = () => {
-  return 'example';
-};
+// Función para enlistar en tabla indicadores y sus valores del año 2012
 
-window.example = example;*/
-/*const primerPaso =WORLDBANK.PER.indicators;
-let resultadoPrimerPaso=[];
-for(let i=0;i<primerPaso.length;i++){
-  resultadoPrimerPaso.push(primerPaso[i]);
+/* const filterData = (data, condition) => {
+  const filterItems = query => {
+    return fruits.filter((el) =>
+      el.toLowerCase().indexOf(query.toLowerCase()) > -1
+    );
   }
-let segundoPaso = [];
-for(var i=0;i<resultadoPrimerPaso.length;i++){
-segundoPaso.push(resultadoPrimerPaso[i].indicatorCode);
-  }
-let palabra = document.getElementById("palabra").value
-if(palabra==="indicadores"){
-  document.write(segundoPaso.join(""));
-  }
-  window.filter(indicadores,search) = {
-    if(indicadores){
-      if(search){
+  
+}  */
 
+/* const one = WORLDBANK.PER.indicators;
+const two = [];
+const three = [];
+let four = document.getElementById('año');
+let five = document.getElementById('search-input');
+
+const lista = () => {
+  for (let i = 0; i < one.length; i++) {
+    two.push(one[i].indicatorName);
+    three.push(one[i].data[four.value]);
+    let tr = document.createElement('TR'); // Crea una fila
+    let td1 = document.createElement('TD'); // Crea la columna indicadores    
+    let t = document.createTextNode(one[i].indicatorName);
+    let td2 = document.createElement('TD'); // Crea la columna valores
+    let nt = document.createTextNode(one[i].data[four.value]);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    td1.appendChild(t);
+    td2.appendChild(nt);
+    document.getElementById('lista').appendChild(tr);
+  }
+}; */
+/* const ordenar = () => {
+  console.log(two.sort());
+} 
+window.filterIndicators = (two, search) => {
+  if (search) {
+      if (two) {
+          search = search.toLowerCase();
+          return two.filter(indic => indic && user.name && user.name.toLowerCase().indexOf(search) >= 0);
       }
-}*/
-const one=WORLDBANK.PER.indicators;
-  const two=[];
-  const three=[];
-    for(i=0; i<one.length; i++) {
-      two.push(one[i].indicatorName);
-      three.push(one[i].data['2012']);
-      let tr = document.createElement("TR");  
-      let th1 = document.createElement("TH");      // Create a <button> element
-      let t = document.createTextNode(one[i].indicatorName);
-      let th2 = document.createElement("TH");        // Create a <button> element
-      let nt = document.createTextNode(one[i].data['2012']);
-      tr.appendChild(th1);
-      tr.appendChild(th2);
-      th1.appendChild(t);
-      th2.appendChild(nt);
-      document.getElementById('lista').appendChild(tr);
-      // document.getElementById('lista').appendChild(nt);
-      // debugger;
-      
-       
-        
-    };
-    //document.getElementById('salida').innerHTML = `${two}`;
+  }
+  return two;
+} */
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,31 @@
 require('../src/data.js');
 
+/* const input = [
+  'Tasa de participación en la fuerza laboral, total (% de la población total mayor de 15años) (estimación nacional)',
+  'Fuerza laboral con educación intermedia, varones (% de la fuerza laboral masculina)', 
+  'Fuerza laboral con educación intermedia (% del total)', 
+  'Empleo de tiempo parcial, mujeres (% del total de mujeres empleadas)',
+  'Fuerza laboral con educación intermedia, mujeres (% de la fuerza laboral femenina)'
+];
 
-describe('example', () => {
+const output = [
+  'Fuerza laboral con educación intermedia (% del total)', 
+  'Fuerza laboral con educación intermedia, mujeres (% de la fuerza laboral femenina)',
+  'Tasa de participación en la fuerza laboral, total (% de la población total mayor de 15años) (estimación nacional)',
+  'Fuerza laboral con educación intermedia, varones (% de la fuerza laboral masculina)'
+]; */
+
+describe('worldbank.busqueda', () => {
+  it('debería ser una función', () => {
+    expect(typeof busqueda).toBe('function');
+  });
+});
+
+/* describe('example', () => {
   it('is a function', () => {
     expect(typeof example).toBe('function');
   });
 
   it('returns `example`', () => {
     expect(example()).toBe('example');
-  });
-});
+  }); */
