73f30f4106bc72a7a8e49e99c77517bee5aadd3e
krpando <karpando@gmail.com>
2019-01-01 18:00:39 -0500

    Responsive web, cambios aun en trabajo. Iteración

--- src/data.js
+++ src/data.js
@@ -1,42 +1,22 @@
 /* window.WORLDBANK = {
-  busqueda
-};
-esta es una función de ejemplo
-puedes ver como agregamos la función a nuestro objeto global window */
-
+  filterIndicators,
+  filterYears
+}; */ 
+/* global filterYears*/
 // Función filtrado para búsqueda rápida
-function filterIndicators(palabra) {
-  const indicadores = one.map((arr) => {
+function filterIndicators(palabra, data) {
+  const indicadores = data.map((arr) => {
     return arr.indicatorName;
   });
   return indicadores.filter((x) => {
     return x.toLowerCase().indexOf(palabra.toLowerCase()) > -1;
   });
 }
-
-// Pintado de tabla en web
-const one = WORLDBANK.PER.indicators;
-const two = [];
-const three = [];
-let four = document.getElementById('año');
-
-const lista = () => {
-  for (let i = 0; i < one.length; i++) {
-    two.push(one[i].indicatorCode);
-    three.push(one[i].data[four.value]);
-    let tr = document.createElement('TR'); // Crea una fila
-    let td1 = document.createElement('TD'); // Crea la columna indicadores    
-    let txt = document.createTextNode(one[i].indicatorCode);
-    let td2 = document.createElement('TD'); // Crea la columna valores
-    let nt = document.createTextNode(one[i].data[four.value]);
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    td1.appendChild(txt);
-    td2.appendChild(nt);
-    document.getElementById('lista').appendChild(tr);
+// Función filtrado por años
+function filterYears(data, año) {
+  const yearData = [];
+  for (let i = 0; i < data.length; i++) {
+    yearData.push(data[i].data[año]);
   }
-}; 
-
-/* const ordenar = () => {
-  console.log(two.sort());
-} */
+  return yearData;
+}
--- src/main.js
+++ src/main.js
@@ -1,21 +1,13 @@
-/* // Funcionalidad de búsqueda rápida
-document.getElementById('searchBtn').addEventListener('click', () => {
-  let palabraClave = document.getElementById('word');
-  if (filterIndicators(palabraClave.value).join() === '') {
-    document.getElementById('result').innerHTML = 'No se encontraron registros';
-  } else {
-    document.getElementById('result').innerHTML = `
-  ${filterIndicators(palabraClave.value).join('</br>')} 
-` ;  
-  }
-});  */
+/* global filterIndicators*/
+/* global filterYears*/
+/* global WORLDBANK*/
 // Funcionalidad de búsqueda rápida
 document.getElementById('searchBtn').addEventListener('click', () => {
   let palabraClave = document.getElementById('word');
-  if (filterIndicators(palabraClave.value).join() === '') {
+  if (filterIndicators(palabraClave.value, WORLDBANK.PER.indicators).join() === '') {
     document.getElementById('result').innerHTML = 'No se encontraron registros';
   } else {
-    const resultado = filterIndicators(palabraClave.value);
+    const resultado = filterIndicators(palabraClave.value, WORLDBANK.PER.indicators);
     for (let i = 0; i < resultado.length; i++) {
       document.getElementById('result').innerHTML += `
     <ul>
@@ -23,5 +15,20 @@ document.getElementById('searchBtn').addEventListener('click', () => {
     </ul>
   ` ;
     }  
+  }
+});
+// Funcionalidad para mostrar tabla
+document.getElementById('yearBtn').addEventListener('click', () => {
+  let año = document.getElementById('año');
+  const indicadores = WORLDBANK.PER.indicators;
+  const resultadoValores = filterYears(indicadores, parseInt(año.value));
+  for (let i = 0; i < indicadores.length; i++) {
+    const resultadoIndicadores = indicadores[i].indicatorName;
+    document.getElementById('table').innerHTML += `
+    <tr>
+      <td>${resultadoIndicadores}</td>
+      <td>${resultadoValores[i]}</td>
+    </tr>
+  ` ;
   }
 });
