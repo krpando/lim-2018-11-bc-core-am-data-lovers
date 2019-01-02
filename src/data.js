136d0e8fa0dabc3c6c262f25b4e7599c60cdb17c
krpando <karpando@gmail.com>
2019-01-01 19:57:01 -0500

    En data.js función filter para búsqueda rápida y filtrado por años. En main.js código para impresión en pantalla de resultados

--- src/data.js
+++ src/data.js
@@ -1,8 +1,22 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-const example = () => {
-  return 'example';
-};
-
-window.example = example;
+/* window.WORLDBANK = {
+  filterIndicators,
+  filterYears
+}; */ 
+/* global filterYears*/
+// Función filtrado para búsqueda rápida
+function filterIndicators(palabra, data) {
+  const indicadores = data.map((arr) => {
+    return arr.indicatorName;
+  });
+  return indicadores.filter((x) => {
+    return x.toLowerCase().indexOf(palabra.toLowerCase()) > -1;
+  });
+}
+// Función filtrado por años
+function filterYears(data, año) {
+  const yearData = [];
+  for (let i = 0; i < data.length; i++) {
+    yearData.push(data[i].data[año]);
+  }
+  return yearData;
+}
---
+++ src/main.js
@@ -0,0 +1,35 @@
+/* global filterIndicators*/
+/* global filterYears*/
+/* global WORLDBANK*/
+// Funcionalidad de búsqueda rápida
+document.getElementById('searchBtn').addEventListener('click', () => {
+    let palabraClave = document.getElementById('word');
+    if (filterIndicators(palabraClave.value, WORLDBANK.PER.indicators).join() === '') {
+      document.getElementById('result').innerHTML = 'No se encontraron registros';
+    } else {
+      const resultado = filterIndicators(palabraClave.value, WORLDBANK.PER.indicators);
+      for (let i = 0; i < resultado.length; i++) {
+        document.getElementById('result').innerHTML += `
+      <ul>
+        <li>${resultado[i]}</li>
+      </ul>
+    ` ;
+      }  
+    }
+  });
+  // Funcionalidad para mostrar tabla
+  document.getElementById('yearBtn').addEventListener('click', () => {
+    let año = document.getElementById('year');
+    const indicadores = WORLDBANK.PER.indicators;
+    const resultadoValores = filterYears(indicadores, parseInt(año.value));
+    for (let i = 0; i < indicadores.length; i++) {
+      const resultadoIndicadores = indicadores[i].indicatorName;
+      if(resultadoValores[i] != 0) {
+      document.getElementById('table').innerHTML += `
+        <tr>
+            <td>${resultadoIndicadores}</td>
+            <td>${resultadoValores[i]}</td>
+        </tr>
+    ` ;
+    }}
+  });
